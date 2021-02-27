b6bfa48e10904fd3d11d4da709c562b44eff0b72
sedaaggez <sedaaggez.koctas@gmail.com>
2021-02-27 22:39:18 +0300

    Firebase connect Create Add ToDo Item Dialog

--- app/src/main/java/com/sedaaggez/todolist/view/ToDoListFragment.kt
+++ app/src/main/java/com/sedaaggez/todolist/view/ToDoListFragment.kt
@@ -1,19 +1,22 @@
 package com.sedaaggez.todolist.view
 
+import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import android.widget.Button
+import android.widget.EditText
 import com.sedaaggez.todolist.R
 import kotlinx.android.synthetic.main.fragment_to_do_list.*
 
 class ToDoListFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_to_do_list, container, false)
@@ -22,10 +25,29 @@ class ToDoListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        button.setOnClickListener{
-            val action = ToDoListFragmentDirections.actionToDoListFragmentToToDoItemFragment()
-            Navigation.findNavController(it).navigate(action)
+        floatingActionButton.setOnClickListener {
+            showDialog(requireContext())
         }
+    }
+
+
+    private fun showDialog(context: Context) {
+        val dialog = Dialog(context)
+        dialog.setContentView(R.layout.dialog_add_to_do_item)
+        val editTextItemTitle = dialog.findViewById(R.id.editTextItemTitle) as EditText
+        val editTextItemDescription = dialog.findViewById(R.id.editTextItemDescription) as EditText
+        val buttonSave = dialog.findViewById(R.id.buttonSave) as Button
+        val buttonCancel = dialog.findViewById(R.id.buttonCancel) as Button
+        buttonCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        buttonSave.setOnClickListener {
+            println(editTextItemDescription.text)
+            println(editTextItemTitle.text)
+            dialog.dismiss()
+        }
+        dialog.show()
+
     }
 
 }
